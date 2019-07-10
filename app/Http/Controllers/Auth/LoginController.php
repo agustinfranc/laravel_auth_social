c607e698cc1613bcd48fafe5e798ee3fd648fd6c
Agustin <agustin.amato96@gmail.com>
2019-07-10 19:45:28 -0300

    login con facebook

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Socialite;
+use App\User;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        $userSocial = Socialite::driver($provider)->user();
+
+        $findUser = User::where('email', $userSocial-> email)->first();
+
+        if ($findUser) {
+
+            Auth::login($findUser);
+
+            return view('home');
+        }
+        else {
+
+            $user = new User;
+            $user->name = $userSocial->name;
+            $user->email = $userSocial->email;
+            $user->password = bcrypt(84513165);
+            $user->save();
+
+            Auth::login($user);
+
+            return view('home');
+        }
+
+        //return ($user->getId());
+    }
+}
